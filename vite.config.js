4e3277793cd3fc929e634a5d7edfde3422555330
stoyangalchev <galchev98@gmail.com>
2024-10-10 21:49:57 +0300

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: "/external/my-lib.mjs",
+    },
+  }
+})
